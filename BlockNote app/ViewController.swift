3636e82cb1e42fd0e746606515acc83f0dbdbb71
kovs705 <smartkovsik@gmail.com>
2021-11-13 23:34:59 +0300

    Initial Commit

---
+++ BlockNote app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlockNote app
+//
+//  Created by Kovs on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
