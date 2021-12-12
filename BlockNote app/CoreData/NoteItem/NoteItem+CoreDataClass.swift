13fda79a63ca1523adcdc22ebb3ad8eb5a676c94
kovs705 <smartkovsik@gmail.com>
2021-12-12 17:52:52 +0300

    fixed files

---
+++ BlockNote app/CoreData/NoteItem/NoteItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Noteitem+CoreDataClass.swift
+//  BlockNote app
+//
+//  Created by Kovs on 14.11.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+// @objc(Noteitem)
+public class NoteItem: NSManagedObject {
+    @NSManaged var noteItemOrder: Int
+}
---
+++ BlockNote app/CoreData/NoteItem/NoteItem+CoreDataProperties.swift
@@ -0,0 +1,38 @@
+//
+//  Noteitem+CoreDataProperties.swift
+//  BlockNote app
+//
+//  Created by Kovs on 14.11.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension NoteItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<NoteItem> {
+        return NSFetchRequest<NoteItem>(entityName: "NoteItem")
+    }
+
+    @NSManaged public var itemType: String?
+    @NSManaged public var noteItemName: String?
+    
+    @NSManaged public var noteItemText: String?
+    @NSManaged public var note: Note?
+    
+    public var wrappedNoteItemName: String {
+        noteItemName ?? "Unknown NoteItem name"
+    }
+    public var wrappedNoteItemText: String {
+        noteItemText ?? "Unknown text"
+    }
+    public var wrappedNoteItemOrder: Int {
+        noteItemOrder
+    }
+}
+
+extension NoteItem : Identifiable {
+
+}
