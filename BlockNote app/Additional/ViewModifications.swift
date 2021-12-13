f7045ea9f6d74efeb36560a6c955af33cc9f7697
kovs705 <smartkovsik@gmail.com>
2021-12-13 12:10:34 +0300

    working on MainPage

---
+++ BlockNote app/Additional/ViewModifications.swift
@@ -0,0 +1,52 @@
+//
+//  RoundedCornerView.swift
+//  BlockNote app
+//
+//  Created by Kovs on 12.12.2021.
+//
+
+import UIKit
+
+@IBDesignable
+class ViewModifications: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+}
+    
+@IBDesignable extension UIView {
+    @IBInspectable var shadowRadius: CGFloat {
+        get { return layer.shadowRadius }
+        set { layer.shadowRadius = newValue }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat {
+        get { return CGFloat(layer.shadowOpacity) }
+        set { layer.shadowOpacity = Float(newValue) }
+    }
+
+    @IBInspectable var shadowOffset: CGSize {
+        get { return layer.shadowOffset }
+        set { layer.shadowOffset = newValue }
+    }
+
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            guard let cgColor = layer.shadowColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+        set { layer.shadowColor = newValue?.cgColor }
+    }
+}
--- BlockNote app/UIKit Views/ViewController.swift
+++ BlockNote app/UIKit Views/ViewController.swift
@@ -7,16 +7,25 @@
 
 import UIKit
 import CoreData
+import SwiftUI
 
 class ViewController: UIViewController {
     
     // lazy var coreDataStack = CoreDataStack(modelName: "BlockNote_app")
     // let mainPageView = UIHostingController(rootView: C1NavigationView())
+    @IBOutlet weak var statView: UIView!
     
     var groups: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        statView.layer.shadowColor = UIColor(Color.lightPart).cgColor
+        statView.layer.shadowOpacity = 0.2
+        statView.layer.shadowOffset = CGSize(width: 5, height: 15)
+        statView.layer.shadowRadius = 5.0
+        
+        statView.layer.masksToBounds = false
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let viewContext = appDelegate.persistentContainerOffline.viewContext
