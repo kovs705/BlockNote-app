6dcefbdfaaff97f24f2fd1d19f163a1f7ca7e82a
kovs705 <smartkovsik@gmail.com>
2022-01-15 22:15:26 +0300

    new elements, files

---
+++ BlockNote app/C1UIKit/UIKit views/C1NavViewCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  C1NavViewCollectionViewCell.swift
+//  BlockNote app
+//
+//  Created by Kovs on 15.01.2022.
+//
+
+import UIKit
+
+class C1NavViewCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var groupName: UILabel!
+    @IBOutlet weak var numberOfNotes: UILabel!
+}
--- BlockNote app/C1UIKit/UIKit views/C1NavigationViewController.swift
+++ BlockNote app/C1UIKit/UIKit views/C1NavigationViewController.swift
@@ -7,26 +7,34 @@
 
 import UIKit
 
-class C1NavigationViewController: UIViewController {
+class C1NavigationViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     ///
     /// создать ScrollView
     /// добавить containerView с SwiftUI объектами, взяв их из файлов
     /// разобраться где будет UIViewController, агде UIHostingController (проблема с dismiss view)
     ///
-    ///
-    ///
-    ///
 
+    let identifierForCollectionCell = "groupCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
