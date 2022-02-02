94cbf1ec0ff6b56ead07c2249675cdcd55c566a8
kovs705 <smartkovsik@gmail.com>
2022-02-02 16:23:47 +0300

    working on programmatic UI

--- BlockNote app/C1UIKit/UIKit views/C1GroupDetailView.swift
+++ BlockNote app/C1UIKit/UIKit views/C1GroupDetailView.swift
@@ -5,93 +5,89 @@
 //  Created by Kovs on 23.01.2022.
 //
 
+
 import UIKit
 import CoreData
 import SwiftUI
 
+
+
 class C1GroupDetailView: UIViewController {
     
+    #warning("Video - 12:38")
+    
     var groupType = GroupType()
+    var mainView: MainView { return self.view as! MainView }
     
-    let scrollView = UIScrollView()
-    let contentView = UIView()
-    
-    // left = 40    right = 40      top = 20    height 150      width = 310
+    // let scrollView = UIScrollView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // let groupChildView = UIHostingController(rootView: GroupBar(groupType: groupType))
-//        addChild(groupChildView)
-//        groupChildView.view.frame = containerBar.bounds
-//        containerBar.addSubview(groupChildView.view)
-//        groupChildView.didMove(toParent: self)
-        
-        setupScrollView()
-        setupViews()
-        // setupSwiftUIBarViewToTheContainerView()
-        
         title = groupType.groupName ?? "Unknown"
         
-        // MARK: - Test
-        print("Name: \(groupType.wrappedGroupName)")
-        print("Number of the group: \(groupType.wrappedNumber)")
-        // print("Color of the group: \(groupType.groupColor)")
-        
     }
     
-    func setupScrollView() {
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func loadView() {
+        self.view = MainView(frame: UIScreen.main.bounds)
+    }
+    
+    
+    
+}
+
+class MainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .black
+        setupViews()
+        setupScrollViewConstraints()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        self.addSubview(mainScrollView)
+        mainScrollView.addSubview(contentView)
+    }
+    
+    fileprivate func setupScrollViewConstraints() {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        mainScrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainScrollView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20).isActive = true
+        mainScrollView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        mainScrollView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
+        mainScrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    func setupConstraints() {
         contentView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(scrollView)
-        scrollView.addSubview(contentView)
-        contentView.backgroundColor = .black
-        
-        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
-        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
-        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
     }
     
-    let titleLabel: UILabel = {
-           let label = UILabel()
-           label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-           label.numberOfLines = 0
-           label.sizeToFit()
-           label.textColor = UIColor.white
-           label.translatesAutoresizingMaskIntoConstraints = false
-           return label
-       }()
-       
-       let subtitleLabel: UILabel = {
-           let label = UILabel()
-           label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-           label.numberOfLines = 0
-           label.sizeToFit()
-           label.textColor = UIColor.white
-           label.translatesAutoresizingMaskIntoConstraints = false
-           return label
-       }()
     
     
-    func setupViews(){
-        contentView.addSubview(titleLabel)
-        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3.5/4).isActive = true
-        
-        contentView.addSubview(subtitleLabel)
-        subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
-        subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3.5/4).isActive = true
-        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-    }
+    let contentView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .green
+        return view
+    }()
     
+    let mainScrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        scrollView.backgroundColor = .white
+        scrollView.alwaysBounceVertical = true
+        return scrollView
+    }()
     
 }
