05add39ca16e123a79961a5b6e97aea6204fa0ab
kovs705 <smartkovsik@gmail.com>
2022-02-02 14:00:03 +0300

    trying to change UILabels to UIView with SwiftUI bar and list with notes

--- BlockNote app/C1UIKit/UIKit views/C1GroupDetailView.swift
+++ BlockNote app/C1UIKit/UIKit views/C1GroupDetailView.swift
@@ -28,7 +28,8 @@ class C1GroupDetailView: UIViewController {
 //        groupChildView.didMove(toParent: self)
         
         setupScrollView()
-        setupSwiftUIBarViewToTheContainerView()
+        setupViews()
+        // setupSwiftUIBarViewToTheContainerView()
         
         title = groupType.groupName ?? "Unknown"
         
@@ -58,20 +59,38 @@ class C1GroupDetailView: UIViewController {
         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
     }
     
-    let swiftUIBar: UIView = {
-        let containerView = UIView()
-        containerView.backgroundColor = .black
-        containerView.sizeThatFits(CGSize(width: UIScreen.main.bounds.width - 40, height: 150))
-        containerView.sizeToFit()
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        return containerView
-    }()
+    let titleLabel: UILabel = {
+           let label = UILabel()
+           label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+           label.numberOfLines = 0
+           label.sizeToFit()
+           label.textColor = UIColor.white
+           label.translatesAutoresizingMaskIntoConstraints = false
+           return label
+       }()
+       
+       let subtitleLabel: UILabel = {
+           let label = UILabel()
+           label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+           label.numberOfLines = 0
+           label.sizeToFit()
+           label.textColor = UIColor.white
+           label.translatesAutoresizingMaskIntoConstraints = false
+           return label
+       }()
     
-    func setupSwiftUIBarViewToTheContainerView() {
-        contentView.addSubview(swiftUIBar)
-        swiftUIBar.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        swiftUIBar.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        swiftUIBar.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+    
+    func setupViews(){
+        contentView.addSubview(titleLabel)
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3.5/4).isActive = true
+        
+        contentView.addSubview(subtitleLabel)
+        subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
+        subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3.5/4).isActive = true
+        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
     
     
