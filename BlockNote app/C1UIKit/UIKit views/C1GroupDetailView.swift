0df31ab9b0d1c57c1f0860d0a435fa1d5a99748b
kovs705 <smartkovsik@gmail.com>
2022-02-08 12:44:33 +0300

    some changes

--- BlockNote app/C1UIKit/UIKit views/C1GroupDetailView.swift
+++ BlockNote app/C1UIKit/UIKit views/C1GroupDetailView.swift
@@ -20,19 +20,23 @@ class C1GroupDetailView: UIViewController {
     // MARK: - Views
     lazy var scrollView: UIScrollView = {
         let view = UIScrollView(frame: .zero)
-        view.backgroundColor = .green
+        view.backgroundColor = .white
         view.frame = self.view.bounds
         view.contentSize = contentViewSize
+        //view.edgesToSuperview()
         view.autoresizingMask = .flexibleHeight
         view.showsVerticalScrollIndicator = true
+        view.alwaysBounceHorizontal = false
+        view.alwaysBounceVertical = true
         view.bounces = true
         return view
     }()
     
     lazy var containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black
+        view.backgroundColor = .white
         view.frame.size = contentViewSize
+        // view.edges(to: scrollView)
         return view
     }()
     
@@ -41,7 +45,7 @@ class C1GroupDetailView: UIViewController {
         text.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         text.numberOfLines = 0
         text.sizeToFit()
-        text.textColor = UIColor.white
+        text.textColor = UIColor.black
         return text
     }()
     
@@ -51,12 +55,16 @@ class C1GroupDetailView: UIViewController {
         
         title = groupType.groupName ?? "Unknown"
         
-        view.addSubview(scrollView)
-        scrollView.addSubview(containerView)
-        
-        containerView.addSubview(loremIpsumLabel)
-        loremIpsumLabel.center(in: containerView)
-        loremIpsumLabel.edges(to: containerView, insets: .top(20) + .left(20) + .right(20))
+//        view.addSubview(scrollView)
+//        scrollView.addSubview(containerView)
+//
+//        containerView.addSubview(loremIpsumLabel)
+//
+////        loremIpsumLabel.center(in: containerView)
+//        containerView.edges(to: scrollView)
+//        loremIpsumLabel.top(to: containerView)
+//        loremIpsumLabel.edges(to: containerView, insets: .top(20) + .left(20) + .right(20))
+//        loremIpsumLabel.bottom(to: containerView)
         
     }
     
@@ -64,6 +72,26 @@ class C1GroupDetailView: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    
+    func setupViews() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+        containerView.backgroundColor = .black
+        
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        containerView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        
+        
+        
+    }
     
 }
