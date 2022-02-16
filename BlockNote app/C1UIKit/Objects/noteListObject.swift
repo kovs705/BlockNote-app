de0dc834fbbb1464ae01b2050337fa1843cff0ea
kovs705 <smartkovsik@gmail.com>
2022-02-16 14:46:54 +0300

    working on object for the noteList in DetailView

---
+++ BlockNote app/C1UIKit/Objects/noteListObject.swift
@@ -0,0 +1,35 @@
+//
+//  noteListObject.swift
+//  BlockNote app
+//
+//  Created by Kovs on 16.02.2022.
+//
+
+import UIKit
+import CoreData
+import TinyConstraints
+import SnapKit
+
+
+class noteListObject: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // setup UI function here..
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // same setup function here
+    }
+    
+    func setupObject() {
+        // https://www.youtube.com/watch?v=_DADWRicrGU
+        //  0:28 ------> do it next
+    }
+    
+    
+    
+    
+}
+
