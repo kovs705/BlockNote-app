eeaed8c20e368eda4adb9feb84682869d45b293d
kovs705 <smartkovsik@gmail.com>
2022-03-18 00:27:28 +0300

    little changes for appearance of the app + changed a direction of some files

--- BlockNote app/AppDelegate.swift
+++ BlockNote app/AppDelegate.swift
@@ -12,7 +12,11 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Customization
+        
+        UINavigationBar.appearance().backgroundColor = UIColor(named: "DarkBackground")
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(named: "TextForegroundColor") ?? .white]
+        
         
         return true
     }
---
+++ BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
@@ -0,0 +1,10 @@
+//
+//  C1NoteView.swift
+//  BlockNote app
+//
+//  Created by Kovs on 17.03.2022.
+//
+
+import Foundation
+import CoreData
+
