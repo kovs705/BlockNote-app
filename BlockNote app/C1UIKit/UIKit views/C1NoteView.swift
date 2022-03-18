1c0de8225e16c6e09d7ec1aa04814f1d84e43644
kovs705 <smartkovsik@gmail.com>
2022-03-19 01:36:13 +0300

    changed lines, will test it later

--- BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
+++ BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
@@ -14,8 +14,11 @@ class C1NoteView: UIViewController, ConstraintRelatableTarget {
     
     var noteType = Note()
     
-    lazy var contentView = UIView()
-    lazy var scrollView = UIScrollView()
+    lazy var contentView    = UIView()
+    lazy var scrollView     = UIScrollView()
+    
+    lazy var blackView      = UIView()
+    lazy var redView        = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,57 +26,44 @@ class C1NoteView: UIViewController, ConstraintRelatableTarget {
         title = noteType.wrappedNoteName
         self.view.backgroundColor = .white
         
-    }
-    
-    scrollView.bounces         = true
-    scrollView.backgroundColor = UIColor(named: "DarkBackground")
-    self.view.addSubview(scrollView)
-    
-    scrollView.snp.makeConstraints { (make) -> Void in
-        make.edges.equalTo(self)
-    }
-    
-    scrollView.addSubview(contentView)
-    
-    contentView.snp.makeConstraints { (make) -> Void in
-        make.edges.equalTo(scrollView)
-        make.width.equalTo(self)
-    }
-    
-    // Make sure each view contained by the content view
-    // has an height (intrisic or specified)
-    
-    
-    
-    
-    
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.setupHierarchy()
-    }
-    
-    private func setupHierarchy() {
-
-        let blackView = UIView()
-        blackView.backgroundColor = UIColor.black
-        contentView.addSubview(blackView)
-        blackView.snp.makeConstraints { make in
-            // The first child view must be pinned to the top
-            // of the content view
-            make.top.width.equalTo(contentView)
-            make.height.equalTo(1000)
+        
+        
+        scrollView.bounces         = true
+        scrollView.backgroundColor = UIColor(named: "DarkBackground")
+        self.view.addSubview(scrollView)
+        
+        scrollView.snp.makeConstraints { (make) -> Void in
+            make.edges.equalTo(self)
         }
         
-        let redView = UIView()
+        scrollView.addSubview(contentView)
+        
+        contentView.snp.makeConstraints { (make) -> Void in
+            make.edges.equalTo(scrollView)
+            make.width.equalTo(self)
+        }
+        
+        // Make sure each view contained by the content view
+        // has an height (intrisic or specified)
+        
+        
+        blackView.backgroundColor = UIColor.black
+        contentView.addSubview(blackView)
+        
+        blackView.snp.makeConstraints { (make) -> Void in
+            // should be on top:
+            make.top.width.equalTo(contentView)
+            make.height.equalTo(1000) // <---- fot the test
+        }
+        
         redView.backgroundColor = UIColor.red
         contentView.addSubview(redView)
-        redView.snp.makeConstraints { make in
+        
+        redView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(blackView.snp.bottom)
             make.width.equalTo(contentView)
             make.height.equalTo(1000)
-            // The last child view must be pinned to the bottom
-            // of the content view
+            // pin to the bottom:
             make.bottom.equalTo(contentView.snp.bottom)
         }
     }
