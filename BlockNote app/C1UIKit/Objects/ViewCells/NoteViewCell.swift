dd6e21d0cc1c52aeb7a74b68869542e07bdf9a29
kovs705 <smartkovsik@gmail.com>
2022-03-28 19:34:07 +0300

    made a little concept for the noteCell

--- BlockNote app/C1UIKit/Objects/ViewCells/NoteViewCell.swift
+++ BlockNote app/C1UIKit/Objects/ViewCells/NoteViewCell.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
 class NoteViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var noteName: UILabel!
+    @IBOutlet weak var chevronRight: UIImageView!
+    
+    var groupType = GroupType()
+    var noteType = Note()
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        chevronRight.tintColor = UIColor(named: groupType.groupColor ?? "TextForegroundColor")
+        
         
     }
 
