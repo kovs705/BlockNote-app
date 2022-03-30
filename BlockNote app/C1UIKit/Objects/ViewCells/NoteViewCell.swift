b99d78b08d65369e32efb5403191966407fd4199
kovs705 <smartkovsik@gmail.com>
2022-03-30 16:15:20 +0300

    added outlets for cells

--- BlockNote app/C1UIKit/Objects/ViewCells/NoteViewCell.swift
+++ BlockNote app/C1UIKit/Objects/ViewCells/NoteViewCell.swift
@@ -13,14 +13,20 @@ class NoteViewCell: UICollectionViewCell {
     @IBOutlet weak var noteName: UILabel!
     @IBOutlet weak var chevronRight: UIImageView!
     
-    var groupType = GroupType()
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    // var groupType = GroupType()
     var noteType = Note()
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // chevronRight.tintColor = UIColor(named: groupType.groupColor ?? "TextForegroundColor")
-        
+        noteLabel.text = noteType.noteName
         
     }
 
