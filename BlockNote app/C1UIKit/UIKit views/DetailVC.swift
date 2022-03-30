67f42e43a6ae55bfd3eafcda08162c9ae6a5265f
kovs705 <smartkovsik@gmail.com>
2022-03-30 23:25:29 +0300

    added code to add note

--- BlockNote app/C1UIKit/UIKit views/DetailVC.swift
+++ BlockNote app/C1UIKit/UIKit views/DetailVC.swift
@@ -26,6 +26,9 @@ class DetailVC: UIViewController {
 // properties
     lazy var groupType = GroupType()
     lazy var noteObject = Note()
+    
+    var groupTypeSorted: [GroupType] = [GroupType]()
+    
 // segue
     let segueToNoteView = "showNoteView"
 // CoreData properties
@@ -35,7 +38,7 @@ class DetailVC: UIViewController {
     let noteIsMarked = "noteIsMarked"
     
 
-    // MARk: - did load
+    // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -46,6 +49,7 @@ class DetailVC: UIViewController {
 
 }
 
+// MARK: - UICollectinView
 extension DetailVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -67,6 +71,7 @@ extension DetailVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayo
         CGSize(width: (collectionView.frame.width-4)/1, height: 50)
     }
     
+    // MARK: - Setup UI
     func setupDetailVC() {
         scrollView.alwaysBounceVertical = true
         scrollView.bounces = true
@@ -82,4 +87,111 @@ extension DetailVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayo
         third_column.backgroundColor = UIColor(named: "\(groupType.groupColor ?? "BlueBerry")")
     }
     
+    // MARK: - Update UI / Change UI
+    //
+    //
+    //
+    //
+    
+    // MARK: - Alert with textField to add the note
+    @objc func addNote() {
+        let alert = UIAlertController(title: "New Note", message: "Enter a name for the note", preferredStyle: .alert)
+        
+        // save button
+        let saveNoteButton = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            
+            guard
+                let textField = alert.textFields?.first,
+                let noteToSave = textField.text
+            else {
+                print("Note has not been saved")
+                return
+            }
+            // save action:
+            self.saveNote(noteName: noteToSave) // -----------> Check this
+            
+        }
+        // cancel button
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveNoteButton)
+        alert.addAction(cancelButton)
+        
+        present(alert, animated: true)
+    }
+    
+    // MARK: - Save the note to the group from the Adding alert
+    func saveNote(noteName: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        // use viewContext to save changes:
+        let viewContext = appDelegate.persistentContainerOffline.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Note", in: viewContext)!
+        
+        let note = NSManagedObject(entity: entity, insertInto: viewContext)
+        
+        if groupType.wrappedGroupName == "" {
+            note.setValue("Unknown group", forKey: "noteType")
+        } else {
+            note.setValue(self.groupType.wrappedGroupName, forKey: "noteType")
+        }
+        
+        if groupType.typesOfNoteArray.isEmpty {
+            note.setValue(1, forKey: "noteID")
+        } else {
+            note.setValue((groupType.typesOfNoteArray.last?.noteID ?? 0) + 1, forKey: "noteID")
+            print("\((groupType.typesOfNoteArray.count) + 1) note added already")
+        }
+        
+        // TODO: When i delete or resort, I should update every note to make sure every note has its own noteID
+        
+        note.setValue("Test level", forKey: "noteLevel")
+        
+        if noteName == "" || noteName.isEmpty {
+            acceptAttention()
+            return
+        } else {
+            note.setValue(noteName, forKey: "noteName")
+        }
+        // Append the note to the group:
+        do {
+            // adding object
+            self.groupType.addObject(value: note, forKey: "noteTypes")
+            // saving changes
+            if self.groupType.hasChanges {
+                try viewContext.save()
+            } else {
+                fatalError("Just testing if something will go wrong, delete it after some time")
+            }
+            
+            // updating layout of UITableView with notes:
+            
+            
+            
+//            self.noteListCollection.layoutIfNeeded()
+//            self.notesTableView.updateConstraints()
+//            self.noteListCollection.reloadData()
+            
+        } catch let error as NSError {
+            print("Could not save and add note. \(error), \(error.userInfo)")
+        }
+        
+        //        noteName  noteLevel   noteType    noteItems   noteIsMarked
+        //        typeOfNote    wrappedNoteType     wrappedNoteName     noteItemArray
+    }
+    
+    // MARK: - Accept the warning and open the alert again
+    func acceptAttention() {
+        let attentionAlert = UIAlertController(title: "Enter note name", message: "Type something in field", preferredStyle: .alert)
+        
+        let acceptButton = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.addNote()
+        }
+        
+        attentionAlert.addAction(acceptButton)
+        present(attentionAlert, animated: true)
+    }
 }
