3e8d90e55389fad24b6bd34ab4ee02a3f26d203d
kovs705 <smartkovsik@gmail.com>
2022-04-25 22:10:31 +0300

    working on preparing C1NoteView.swift for code

--- BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
+++ BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
@@ -8,18 +8,37 @@
 import CoreData
 import UIKit
 
-class C1NoteView: UIViewController {
+class C1NoteView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     lazy var note = Note()
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topNoteName: UITextView!
+    @IBOutlet weak var blockCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Navigation and ScrollView
         title = note.wrappedNoteName
         scrollView.alwaysBounceVertical = true
         
+        // UICollectionView
+        blockCollectionView.allowsSelection = true
+        blockCollectionView.dataSource      = self
+        blockCollectionView.delegate        = self
         
+        
+        
+        
+    }
+    
+    // MARK: - UICollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        note.noteItemArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
     
     
