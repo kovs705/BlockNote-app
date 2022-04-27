5fe89f1f297d318e98240550d19e1006a01fa7fc
kovs705 <smartkovsik@gmail.com>
2022-04-27 12:13:31 +0300

    working on textBlock cell + put some files in folders to organise the project

--- BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
+++ BlockNote app/C1UIKit/UIKit views/C1NoteView.swift
@@ -8,12 +8,17 @@
 import CoreData
 import UIKit
 
-class C1NoteView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class C1NoteView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     lazy var note = Note()
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var topNoteName: UITextView!
     @IBOutlet weak var blockCollectionView: UICollectionView!
+    
+    var noteItemArray_Sorted: [NoteItem] = []
+    
+    // blockTypes:
+    let textBlock = "TextBlock"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +26,9 @@ class C1NoteView: UIViewController, UICollectionViewDataSource, UICollectionView
         // Navigation and ScrollView
         title = note.wrappedNoteName
         scrollView.alwaysBounceVertical = true
+        
+        // Sorting and other stuff
+        noteItemArray_Sorted = note.noteItemArray.sorted { $0.noteItemOrder < $1.noteItemOrder }
         
         // UICollectionView
         blockCollectionView.allowsSelection = true
@@ -38,11 +46,31 @@ class C1NoteView: UIViewController, UICollectionViewDataSource, UICollectionView
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let noteItem = noteItemArray_Sorted[indexPath.row]
+        
+        if noteItem.noteItemType == textBlock {
+            let textBlockCell = collectionView.dequeueReusableCell(withReuseIdentifier: <#T##String#>, for: <#T##IndexPath#>)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         <#code#>
     }
     
     
     
+    
 }
+
+/*
+ To continue I need to know what type of cells user will have
+    taskBlock - list of tasks
+    vocabularyBlock - show the translation by clicking on word
+    mapBlock - attach some coordinates on map (share - open in Google Maps)
+ TextBlock  <------ in progress
+    ImageBlock
+ ======================
+ make a grey colored loading on the upper right corner
+ */
 
 
