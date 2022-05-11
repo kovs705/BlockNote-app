75e1d568cf07c47dc691acd8ee318a51363aefd1
kovs705 <smartkovsik@gmail.com>
2022-05-11 23:42:51 +0300

    working on height of the cells in UITableView programmatically

--- BlockNote app/C1UIKit/UIKit views/ProgrammaticUI/C1NoteDetailView.swift
+++ BlockNote app/C1UIKit/UIKit views/ProgrammaticUI/C1NoteDetailView.swift
@@ -23,6 +23,7 @@ import SnapKit
 
 class C1NoteDetailView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
     var textBlock = "TextBlock"
     
     // MARK: - Views
@@ -39,14 +40,17 @@ class C1NoteDetailView: UIViewController, UITableViewDelegate, UITableViewDataSo
         
         title = note.wrappedNoteName
         self.view.backgroundColor = .white
+        blockTableView.delegate = self
+        blockTableView.dataSource = self
+        blockTableView.register(UITableViewCell.self, forCellReuseIdentifier: textBlock)
+        blockTableView.bounces = false
+        blockTableView.isScrollEnabled = false
         
         // let rightAddButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(self.addBlockNote))
         
         // self.navigationItem.rightBarButtonItem = rightAddButton
         
         // MARK: - ScrollView
-        scrollView.bounces                      = true
-        // scrollView.isPagingEnabled = true
         scrollView.contentSize                  = CGSize(width: Int(self.view.frame.size.width), height: 100)
         scrollView.showsVerticalScrollIndicator = false
         scrollView.contentSize                  = self.view.frame.size
@@ -60,9 +64,43 @@ class C1NoteDetailView: UIViewController, UITableViewDelegate, UITableViewDataSo
             make.top.equalTo(view.snp.top)
             make.bottom.left.right.equalTo(view)
         }
+        
+        scrollView.addSubview(blockTableView)
+        blockTableView.backgroundColor = UIColor(named: "DarkBackground")
+        
+        blockTableView.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(scrollView.snp.width).offset(-40)
+            make.top.equalTo(scrollView.snp.top).offset(25)
+            // TODO: Place a TextView changable title on top of UITableView
+            
+            if !noteItemArray_Sorted.isEmpty {
+                make.height
+            }
+        }
+        
     }
     
     
+    // MARK: UITableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteItemArray_Sorted.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let noteItem = noteItemArray_Sorted[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: textBlock, for: indexPath) as! TVTextBlock
+        
+        cell.textChanged { [weak tableView] (newText: String) in
+            noteItem.noteItemText = newText
+            
+            DispatchQueue.main.async {
+                tableView?.beginUpdates()
+                tableView?.endUpdates()
+            }
+        }
+        return cell
+    }
     
 }
         
