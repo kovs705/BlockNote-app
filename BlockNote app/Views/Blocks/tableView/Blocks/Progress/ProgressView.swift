13edad8253bc2e585820f2bba7264931591ef55f
Kovs <smartkovsik@gmail.com>
2023-09-06 09:29:54 +0300

    working on progress block

---
+++ BlockNote app/Views/Blocks/tableView/Blocks/Progress/ProgressView.swift
@@ -0,0 +1,40 @@
+//
+//  ProgressView.swift
+//  BlockNote app
+//
+//  Created by user on 06.09.2023.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    
+    var startDate: Date
+    var endDate: Date
+    
+    var body: some View {
+        HStack {
+            CircularView(startDate: startDate, endDate: endDate)
+            
+            Spacer()
+            
+            Button(action: {
+                //
+            }, label: {
+                ZStack {
+                    Circle()
+                        .fill(Color(uiColor: .systemBackground).opacity(0.1))
+                    Image(systemName: "ellipsis")
+                }
+            })
+        }
+        .frame(width: .infinity, height: 150)
+    }
+}
+
+struct ProgressView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ProgressView(startDate: Date(), endDate: Date())
+    }
+}
